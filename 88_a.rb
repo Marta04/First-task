13edaceb0134bf2d9ee9ed77fbe1ea201b6c391c
Marta <martaolehivna@gmail.com>
2015-05-24 11:03:36 +0300

    My first commit

---
+++ 88_a.rb
@@ -0,0 +1,15 @@
+puts "put n="
+n=gets.chomp
+m=n.to_i**2
+k=0
+while m>0
+	if m%10==3
+		puts "number "+(n.to_i**2).to_s+" includes 3" 
+		k=1
+		break
+	else m=m/10
+	end
+end
+if k==0
+	puts "number "+(n.to_i**2).to_s+" doesn\'t include 3"
+end
