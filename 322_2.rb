b02112b603a1abc220d7c07e6adc9381df711592
Marta <martaolehivna@gmail.com>
2015-05-29 15:46:24 +0300

    modified tasks with rubokop

--- 322_2.rb
+++ 322_2.rb
@@ -1,15 +1,13 @@
-n=10000
-num=[]
-for i in 1..n
-  s=0
-  for j in 1..n
-    if i%j==0
-        s+=j
-    end
-  end    
+n = 100_00
+num = []
+(1..n).each do |i|
+  s = 0
+  (1..n).each do |j|
+    (i % j == 0) && (s += j)
+  end
   num.push(s)
 end
-m=num.max
-ind=num.index(m)+1
-puts "max="+m.to_s
-puts "number="+ind.to_s
+m = num.max
+ind = num.index(m) + 1
+puts 'max=' + m.to_s
+puts 'number=' + ind.to_s
--- 88_b.rb
+++ 88_b.rb
@@ -1,3 +1,9 @@
-puts "put n="
-n=gets.chomp
-puts "the sum of numbers = "+n.reverse
+puts 'put n='
+a = []
+n = gets.chomp
+m = n.to_i
+while m != 0
+  a.push(m % 10)
+  m /= 10
+end
+puts a.join
